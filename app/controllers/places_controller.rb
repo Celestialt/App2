0901f3ea262e0bc69a0d90b551b6faca6175fe2b
Kirstan Fitzptrick <you@example.com>
2017-02-23 22:20:23 +0000

    Added a link to show.html.erb; added destroy action to places_controller.rb; added code to destroy action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,8 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
 
 	def index
-		@places = Place.paginate(page: params[:page], per_page: 5)
-		
+		@places = Place.order("created_at ASC").paginate(page: params[:page], per_page:5)
+
 	end
 
 	def new
@@ -28,6 +28,13 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 		@place.update_attributes(place_params)
 		redirect_to root_path
+	end
+
+	def destroy
+		@place = Place.find(params[:id])
+		@place.destroy
+		redirect_to root_path
+		
 	end
 
 

f1a676610409281e2604714608b7b649b2543d82
Kirstan Fitzptrick <you@example.com>
2017-03-05 07:19:17 +0000

    Modified config/enviroinments/development.rb file; mofidied config/applicatoin.yml file;

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,7 @@ class NotificationMailer < ApplicationMailer
 	default from: 'no-reply@nomsterapp.com'
 
 	def comment_added
-		mail(to: "bkbwsr@aol.com",
-			subject: "A comment has been added to your place.")
-		
+		mail(to: "kirf1891@gmail.com",
+				subject: "A comment has been added to your place")
 	end
 end
